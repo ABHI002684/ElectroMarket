02b1e092c6b649edc11f837615fca609102a020d
ABHI002684 <abhishek002684@gmail.com>
2024-07-26 23:04:49 +0530

    updated server.js file

--- API/server.js
+++ API/server.js
@@ -2,6 +2,7 @@ const express=require('express');
 const mongoose=require('mongoose');
 const bodyParser=require('body-parser');
 const userRouter=require('./routes/user');
+const productRouter=require('./routes/product');
 const app=express();
 const PORT=1000;
 
@@ -9,8 +10,11 @@ app.use(bodyParser.json());
 //home testing route
 app.get('/',(req,res)=>res.json({message:"this is a home route"}));
 
-//use router
+//using user's router
 app.use('/api/user',userRouter);
+
+//using product's router
+app.use('/api/product',productRouter);
 
 mongoose.connect(
     "mongodb+srv://abhishek002684:uR94nwtbxuSoeWj1@cluster0.etykbra.mongodb.net/",{
