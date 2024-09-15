8c70c057375eb5fe20d8f9aebd699f73b6f4182c
ABHI002684 <abhishek002684@gmail.com>
2024-09-15 11:43:31 +0530

    update the app.jsx

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -5,6 +5,15 @@ import ShowProduct from './component/product/ShowProduct';
 import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
 import ProductDetails from './component/product/ProductDetails';
 import Navbar from './component/Navbar';
+import SearchProduct from './component/product/SearchProduct';
+import Register from './component/user/Register';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Login from './component/user/Login';
+import Profile from './component/user/Profile';
+import Cart from './component/Cart';
+import Address from './component/Address';
+
 const App = () => {
   //const {data}=useContext(AppContext)
   return (
@@ -13,7 +22,13 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route path='/' element={<ShowProduct />}/>
+        <Route path='/product/search/:term' element={<SearchProduct />} />
         <Route path='/product/:id' element={<ProductDetails />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/shipping' element={<Address />} />
       </Routes>
     </Router>
   )
